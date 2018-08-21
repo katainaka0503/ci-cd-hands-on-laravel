6ad7695d0356ae598b585c7a473f7a6371987fe2
katainaka0503 <katainaka0503@gmail.com>
2018-08-21 18:52:09 +0900

    ハンズオン中に発覚した問題点の修正

--- laravel/config/database.php
+++ laravel/config/database.php
@@ -66,6 +66,20 @@ return [
             'engine'    => null,
         ],
 
+        'mysql_testing' => [
+            'driver'    => 'mysql',
+            'host'      => env('TEST_DB_HOST', 'localhost'),
+            'database'  => env('TEST_DB_DATABASE', 'forge'),
+            'username'  => env('TEST_DB_USERNAME', 'forge'),
+            'port'      => env('TEST_DB_PORT', 3306),
+            'password'  => env('TEST_DB_PASSWORD', ''),
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+            'strict'    => false,
+            'engine'    => null,
+        ],
+
         'pgsql' => [
             'driver'   => 'pgsql',
             'host'     => env('DB_HOST', 'localhost'),
